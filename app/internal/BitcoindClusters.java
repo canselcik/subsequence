9f6df9ad9329274db19b3425f1cd121835e548c5
Can Selcik <selcik2@illinois.edu>
2015-05-20 17:16:27 -0500

    Fixing account count

--- app/internal/BitcoindClusters.java
+++ app/internal/BitcoindClusters.java
@@ -2,11 +2,9 @@ package internal;
 
 import internal.rpc.BitcoindClientFactory;
 import internal.rpc.BitcoindInterface;
-import com.google.common.primitives.Longs;
 import play.db.DB;
 
 import java.net.URL;
-import java.security.MessageDigest;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,57 +14,53 @@ import java.util.List;
 
 public class BitcoindClusters {
     public static class ClusterInfo {
-        public String id, connString;
+        public int id;
+        public String connString;
         public BitcoindClientFactory factory;
-        public ClusterInfo(String id, String connString, BitcoindClientFactory factory){
+        public int accountCount;
+        public ClusterInfo(int id, String connString, BitcoindClientFactory factory, int accountCount){
             this.id = id;
             this.connString = connString;
             this.factory = factory;
+            this.accountCount = accountCount;
         }
     }
 
-    private static List<ClusterInfo> clusters = new ArrayList<ClusterInfo>();
-    public static int loadBitcoindClusters(){
+    public static List<ClusterInfo> getBitcoindClusters(){
         Connection c = DB.getConnection();
+        List<ClusterInfo> clusters = new ArrayList<>();
         try {
-            PreparedStatement ps = c.prepareStatement("SELECT id, conn_string, rpc_username, rpc_password FROM bitcoind_clusters ORDER BY id ASC");
+            PreparedStatement ps = c.prepareStatement("SELECT id, conn_string, rpc_username, rpc_password, account_count FROM bitcoind_clusters ORDER BY account_count ASC");
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
-                String id = rs.getString("id");
+                Integer id = rs.getInt("id");
                 String connString = rs.getString("conn_string");
                 String rpcUsername = rs.getString("rpc_username");
                 String rpcPassword = rs.getString("rpc_password");
+                Integer clusterSize = rs.getInt("account_count");
                 BitcoindClientFactory bcf = new BitcoindClientFactory(new URL(connString), rpcUsername, rpcPassword);
-                clusters.add( new ClusterInfo(id, connString, bcf) );
+                clusters.add( new ClusterInfo(id, connString, bcf, clusterSize) );
             }
             c.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return clusters.size();
-    }
-
-    private static int assignCluster(String user){
-        try {
-            MessageDigest cript = MessageDigest.getInstance("SHA-1");
-            cript.reset();
-            cript.update(user.getBytes("utf8"));
-            Long i = Longs.fromByteArray(cript.digest());
-            return (int)(i % clusters.size());
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            return -1;
-        }
-    }
-
-    public static List<ClusterInfo> getClusters(){
         return clusters;
     }
 
+    private static ClusterInfo findLeastOccupiedCluster(){
+        List<ClusterInfo> clusters = getBitcoindClusters();
+        if(clusters == null || clusters.size() == 0)
+            return null;
+        return clusters.get(0);
+    }
+
     public static BitcoindInterface getClusterInterface(Integer clusterId){
+        List<ClusterInfo> clusters = getBitcoindClusters();
+        if(clusters == null || clusters.size() == 0)
+            return null;
         for(ClusterInfo i : clusters){
-            if(i.id.equals(String.valueOf(clusterId)))
+            if(i.id == clusterId)
                 return i.factory.getClient();
         }
         return null;
@@ -74,13 +68,32 @@ public class BitcoindClusters {
 
     public static BitcoindInterface getInterface(String user){
         Integer assignment = checkClusterAssignmentFromDB(user);
-        if(assignment == null){
-            assignment = assignCluster(user);
-            boolean writeResult = writeClusterAssignmentToDB(user, Integer.parseInt(clusters.get(assignment).id));
-            if(!writeResult)
-                return null;
+        if(assignment != null)
+            return getClusterInterface(assignment);
+
+        ClusterInfo leastOccupied = findLeastOccupiedCluster();
+        boolean writeResult = writeClusterAssignmentToDB(user, leastOccupied.id);
+        if(!writeResult)
+            return null;
+        else
+            return leastOccupied.factory.getClient();
+    }
+
+    private static boolean incrementClusterAccountCount(int clusterId){
+        Connection c = DB.getConnection();
+        if(c == null)
+            return false;
+        try {
+            PreparedStatement ps = c.prepareStatement("UPDATE bitcoind_clusters SET account_count = account_count+ 1 WHERE id = ?");
+            ps.setInt(1, clusterId);
+            int affectedRows = ps.executeUpdate();
+            c.close();
+            boolean res = (affectedRows == 1);
+            return res;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
         }
-        return clusters.get(assignment).factory.getClient();
     }
 
     private static boolean writeClusterAssignmentToDB(String user, Integer clusterAssignment){
@@ -97,8 +110,8 @@ public class BitcoindClusters {
             ps.setLong(4, 0);
             int affectedRows = ps.executeUpdate();
             c.close();
-            boolean res = (affectedRows == 1);
-            return res;
+            if(affectedRows != 1) return false;
+            return incrementClusterAccountCount(clusterAssignment);
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
@@ -110,19 +123,14 @@ public class BitcoindClusters {
         if(c == null)
             return null;
         try {
-            PreparedStatement ps = c.prepareStatement("SELECT cluster_id FROM account_holders WHERE account_name = ?");
+            PreparedStatement ps = c.prepareStatement("SELECT * FROM account_holders WHERE account_name = ?");
             ps.setString(1, user);
             ResultSet rs = ps.executeQuery();
             if(rs == null) return null;
             if(!rs.next()) return null;
-            Integer result = rs.getInt(1);
+            Integer result = rs.getInt("cluster_id");
             c.close();
-
-            for(int i = 0; i < clusters.size(); i++){
-                if( clusters.get(i).id.equals(String.valueOf(result)) )
-                    return i;
-            }
-            return null;
+            return result;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
--- app/internal/Global.java
+++ app/internal/Global.java
@@ -4,6 +4,7 @@ import play.*;
 import play.db.DB;
 
 import java.sql.*;
+import java.util.List;
 
 
 public class Global extends GlobalSettings {
@@ -11,13 +12,13 @@ public class Global extends GlobalSettings {
         Connection c = DB.getConnection();
         try {
             Statement s = c.createStatement();
-            s.addBatch("CREATE TABLE \"bitcoind_clusters\" (\"id\" serial PRIMARY KEY, \"conn_string\" TEXT, \"rpc_username\" TEXT, \"rpc_password\" TEXT) WITH (OIDS=FALSE);");
+            s.addBatch("CREATE TABLE \"bitcoind_clusters\" (\"id\" serial PRIMARY KEY, \"conn_string\" TEXT, \"rpc_username\" TEXT, \"rpc_password\" TEXT, \"account_count\" int) WITH (OIDS=FALSE);");
             s.addBatch("CREATE TABLE \"account_holders\" (\"account_id\" bigserial PRIMARY KEY, \"account_name\" TEXT, \"cluster_id\" int, \"confirmed_satoshi_balance\" bigint, \"unconfirmed_satoshi_balance\" bigint) WITH (OIDS=FALSE);");
             s.addBatch("ALTER TABLE \"account_holders\" ADD CONSTRAINT account_holders_fk0 FOREIGN KEY (cluster_id) REFERENCES bitcoind_clusters(id);");
-            s.addBatch("CREATE TABLE \"transactions\" (\"internal_txid\" bigserial PRIMARY KEY, \"matched_user_id\" bigint, \"inbound\" BOOLEAN, \"tx_hash\" TEXT, \"confirmed\" BOOLEAN, \"amount_satoshi\" bigint) WITH (OIDS=FALSE);");
+            s.addBatch("CREATE TABLE \"transactions\" (\"internal_txid\" bigserial PRIMARY KEY, \"matched_user_id\" bigint, \"inbound\" BOOLEAN, \"tx_hash\" TEXT UNIQUE, \"confirmed\" BOOLEAN, \"amount_satoshi\" bigint) WITH (OIDS=FALSE);");
             s.addBatch("ALTER TABLE \"transactions\" ADD CONSTRAINT transactions_fk0 FOREIGN KEY (matched_user_id) REFERENCES account_holders(account_id);");
             s.addBatch("CREATE TABLE \"used_txos\" (\"used_txo_id\" bigserial PRIMARY KEY, \"txo\" TEXT, \"new_txid\" TEXT ) WITH (OIDS=FALSE);");
-            s.addBatch("ALTER TABLE \"used_txos\" ADD CONSTRAINT used_txos_fk0 FOREIGN KEY (associated_internaltx_id) REFERENCES transactions(internal_txid);");
+            s.addBatch("ALTER TABLE \"used_txos\" ADD CONSTRAINT used_txos_fk0 FOREIGN KEY (new_txid) REFERENCES transactions(tx_hash);");
             s.addBatch("CREATE INDEX used_txos_txo_idx ON used_txos(txo);");
             s.addBatch("CREATE INDEX transactions_matched_user_id_idx ON transactions(matched_user_id);");
             s.addBatch("CREATE INDEX account_holders_account_name_idx ON account_holders(account_name);");
@@ -63,16 +64,14 @@ public class Global extends GlobalSettings {
         }
 
         Logger.error("Fetching the available clusters...");
-        int clusterCount = BitcoindClusters.loadBitcoindClusters();
-        if(clusterCount == 0){
+        List<BitcoindClusters.ClusterInfo> clusters = BitcoindClusters.getBitcoindClusters();
+        if(clusters.size() == 0){
             Logger.error("Found no clusters defined in your database. Exiting. Please restart after adding your clusters.");
             System.exit(0);
         }
-        Logger.error("Found " + clusterCount + " clusters. Starting service...");
+        Logger.error("Found " + clusters.size() + " clusters. Starting service...");
     }
 
     @Override
-    public void onStop(Application app) {
-
-    }
+    public void onStop(Application app) { }
 }
