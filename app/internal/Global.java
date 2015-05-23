7bd013ded91199b1591beb10ab320e9bc6908ead
Can Selcik <selcik2@illinois.edu>
2015-05-23 00:35:14 -0500

    INFO level instead of ERROR

--- app/internal/Global.java
+++ app/internal/Global.java
@@ -8,24 +8,24 @@ import play.*;
 public class Global extends GlobalSettings {
     @Override
     public void onStart(Application app) {
-        Logger.error("Checking if the database schema is initialized...");
+        Logger.info("Checking if the database schema is initialized...");
         if(!CommonDB.tableExists("account_holders")) {
-            Logger.error("Initializing the schema...");
+            Logger.info("Initializing the schema...");
             if(!CommonDB.initializeSchema()){
                 Logger.error("Failed to initialize schema. Exiting.");
                 System.exit(-1);
             }
         }
 
-        Logger.error("Fetching the available Bitcoind nodes...");
+        Logger.info("Fetching the available Bitcoind nodes...");
         int clusterCount = BitcoindNodes.getNodeCount();
         if(clusterCount == 0){
             Logger.error("Found no nodes defined in your database. Exiting. Please restart after adding your Bitcoind nodes.");
             System.exit(-1);
         }
-        Logger.error("Found " + clusterCount + " clusters. Starting service...");
+        Logger.info("Found " + clusterCount + " clusters. Starting service...");
 
-        Logger.error("Checking if the local bitcoind node (defined by environment vars) is up...");
+        Logger.info("Checking if the local bitcoind node (defined by environment vars) is up...");
         BitcoindInterface localInterface = BitcoindNodes.getLocalNodeInferface();
         if(localInterface == null){
             Logger.error("Failed to connect to the local bitcoind instance... exiting.");
